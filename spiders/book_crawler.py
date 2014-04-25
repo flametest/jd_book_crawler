534fb5607ab15e0ad9f5903375d76883c3e200e4
flametest <flametest@gmail.com>
2014-04-25 23:46:09 +0800

    second commit

--- spiders/book_crawler.py
+++ spiders/book_crawler.py
@@ -5,18 +5,18 @@ from scrapy.selector import Selector
 from JDCrawler.items import JdcrawlerItem
 
 class BookSpider(Spider):
-	"""docstring for BookSpider"""
-	name = "book_price"
-	allowed_domains = ["jd.com"]
-	start_urls = [
-	"http://book.jd.com/"
-	]
-	rules = [Rule(SgmlLinkExtractor(allow=['item.jd.com/\d+']), 'parse')]
+    """docstring for BookSpider"""
+    name = "book_price"
+    allowed_domains = ["jd.com"]
+    start_urls = [
+    "http://book.jd.com/"
+    ]
+    rules = [Rule(SgmlLinkExtractor(allow=['item.jd.com/\d+']), 'parse')]
 
-	def parse(self, response):
-		sel = Selector(response)
-		book = JdcrawlerItem()
-		book["book_title"] = sel.xpath("//div[@id='name']/h1/text()[1]").extract()
-		book["isbn_no"] = sel.xpath("//ul/li[@id='summary-isbn']/div[@class='dd']/text()").extract()
-		book["book_price"] = sel.xpath("//div[@class='dd']/strong/text()").extract()
-		return book
+    def parse(self, response):
+        sel = Selector(response)
+        book = JdcrawlerItem()
+        book["book_title"] = sel.xpath("//div[@id='name']/h1/text()[1]").extract()
+        book["isbn_no"] = sel.xpath("//ul/li[@id='summary-isbn']/div[@class='dd']/text()").extract()
+        book["book_price"] = sel.xpath("//div[@class='dd']/strong/text()").extract()
+        return book
